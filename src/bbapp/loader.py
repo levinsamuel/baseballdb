141bee033c78e34e592577df82fcc995b30afe25
Samuel Levin <levinsamuel000@gmail.com>
2019-03-02 19:17:04 -0800

    add method for getting model fields

--- src/bbapp/loader.py
+++ src/bbapp/loader.py
@@ -14,10 +14,26 @@ batting_maps = {
     '3B': 'triples'
 }
 
+types_to_load = (Player, Batting, Pitching, Fielding)
+
 
 def to_map(keys, vals):
     return {k: (v if v != '' else None) for k, v in zip(keys, vals)}
 
 
 def load_from_directory(root):
+    pt = Path(root)
+
+
+def find_and_label_files(root):
+    pt = Path(root)
+
+
+def _get_fields_for_type(t):
+    """Given a model type, return a list of the DB fields defined on it"""
+    return [fn.name for fn in t._meta.get_fields()]
+
+
+def _find_and_label_files(root):
+
     pt = Path(root)
--- src/bbapp/simpletests/test_loader.py
+++ src/bbapp/simpletests/test_loader.py
@@ -1,6 +1,9 @@
 from django.test import SimpleTestCase
+from django.db.models.fields import Field
 from bbapp import loader
+from bbapp.models import Player, Batting
 import logging
+import inspect
 
 logging.basicConfig()
 log = logging.getLogger('simple_loader_tests')
@@ -13,3 +16,25 @@ class SimpleLoaderTest(SimpleTestCase):
 
         log.debug('load files from directory')
         loader.load_from_directory('.')
+
+    def test_get_model_fields(self):
+
+        player_fields = loader._get_fields_for_type(Player)
+        log.debug('player fields: %s', player_fields)
+
+        self.assertIn('id', player_fields,
+                      'Expected field to be in player field list')
+        self.assertIn('deathCountry', player_fields,
+                      'Expected field to be in player field list')
+        self.assertIn('bbrefID', player_fields,
+                      'Expected field to be in player field list')
+        self.assertNotIn('player_id', player_fields,
+                         'Unexpected field in player field list')
+
+        # Test batting key
+        batting_fields = loader._get_fields_for_type(Batting)
+        log.debug('batting fields: %s', batting_fields)
+        self.assertIn('player', batting_fields,
+                      'Expected field to be in batting field list')
+        self.assertIn('id', batting_fields,
+                      'Expected field to be in batting field list')
